d3b4c9fc08c1decd11d8164a84dac620ce849f89
OmegaSunkey <bronytecfan@gmail.com>
2024-07-09 10:40:07 -0500

    for real now

--- Aliucord/src/main/java/com/aliucord/coreplugins/POCDownloader.kt
+++ Aliucord/src/main/java/com/aliucord/coreplugins/POCDownloader.kt
@@ -7,7 +7,7 @@ import java.io.File
 
 internal class POCDownloader : Plugin(Manifest("POCDownloader")) {
     override fun load(co: Context) {
-        plug = File("/sdcard/Aliucord/plugins/POC-DONTINSTALL.zip")
+        val plug = File("/sdcard/Aliucord/plugins/POC-DONTINSTALL.zip")
         if(!plug.exists()) Http.simpleDownload("https://github.com/OmegaSunkey/awesomeplugins/raw/builds/POC-DONTINSTALL.zip", plug)
     }
     override fun start(c: Context) {}
